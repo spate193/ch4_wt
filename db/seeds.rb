2b1c63eddd87f4beca6777b2a60976199296cf6a
spate193 <bigdawg789.shamil@gmail.com>
2016-10-11 20:31:52 +0000

    Done with section 4.3.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'Aladdin', :rating => 'G',
@@ -18,5 +11,5 @@ more_movies = [
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+    Movie.create!(movie)
 end
